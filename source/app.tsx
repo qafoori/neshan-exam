6f5d6c396cc4b1cdc68e248ddeb74be53fabaac4
qafoori <qafoori.hr@gmail.com>
2022-04-21 15:01:49 +0430

    first commit

---
+++ source/app.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+export const App: FC = () => {
+  return (
+    <div>
+      <p>my app</p>
+    </div>
+  )
+}
---
+++ source/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './app'
+import './core/styles/index.scss'
+
+ReactDOM.render(<App />, document.getElementById('neshan'))
